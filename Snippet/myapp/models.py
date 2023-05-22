b0b8d10083fcd46d907d9e0026a3255affa8fe27
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-05-22 21:29:27 +0530

    22.05.2023

--- Snippet/myapp/models.py
+++ Snippet/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Snippet(models.Model):
+	title=models.CharField(max_length=100)
+	code=models.CharField(max_length=100)
+	linenos=models.BooleanField()
+	language=models.CharField(max_length=50)
+	style=models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.title
--- Snippet/myapp/views.py
+++ Snippet/myapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Snippet
+from rest_framework import generics
+from .serializers import SnippetSerializer
 
 # Create your views here.
