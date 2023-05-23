dc1f141aa0bb4fb8c0eee08b9c7b9762f62af87a
Sagarsonara2143 <sagarsonara2143@gmail.com>
2023-05-23 22:28:05 +0530

    23.05.2023

--- Snippet/myapp/urls.py
+++ Snippet/myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('snippets/',views.snippet_list,name ='snippet-list'),
-	path('snippets/<int:pk>/',views.snippet_detail,name = 'snippet-detail'),
+	path('snippets/',views.snippet_list,name='snippet-list'),
+	path('snippets/<int:pk>/',views.snippet_detail,name='snippet-detail'),
 ]
--- Snippet/myapp/views.py
+++ Snippet/myapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-
+from rest_framework.decorators import api_view
 from django.shortcuts import render
 from .models import Snippet
 from rest_framework import generics
